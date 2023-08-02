997e9507ad2a94abf601209cb5fd49a96ff40c18
AnaJu09 <131875314+AnaJu09@users.noreply.github.com>
2023-08-02 08:41:29 -0300

    consertando o personagem caindo e adicionando variaveis de posição e velocidade

--- Aventuras Rita Palito/Assets/scripts/player.cs
+++ Aventuras Rita Palito/Assets/scripts/player.cs
@@ -6,13 +6,17 @@ public class NewBehaviourScript : MonoBehaviour
 {
     //variavel para o rigidbody
     private Rigidbody2D rig;
+    //vector de posição inicial dp player
+    private Vector3 posInicial;
+    //variavel velocidade
+    private float speed;
 
-
-    // Start is called before the first frame update
+ 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-       
+        posInicial = new Vector3(-3.31, -0.34, 0);
+        transform.position = posInicial;
     }
 
     // Update is called once per frame
