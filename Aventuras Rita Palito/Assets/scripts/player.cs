2683d515bd261c34164268d975bb0925a1ef6345
AnaJu09 <131875314+AnaJu09@users.noreply.github.com>
2023-08-02 09:40:21 -0300

    player virando referente as teclas direita e esquerda

--- Aventuras Rita Palito/Assets/scripts/player.cs
+++ Aventuras Rita Palito/Assets/scripts/player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class player : MonoBehaviour
 {
     //variavel para o rigidbody
     private Rigidbody2D rig;
@@ -15,13 +15,30 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        posInicial = new Vector3(-3.31, -0.34, 0);
+        posInicial = new Vector3(-3.3141f,-0.3481f,0f);
         transform.position = posInicial;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+        //verifica se a tecla a foi pressionada e o valor x da escala esta positivo
+        if (Input.GetKeyDown(KeyCode.A) && transform.localScale.x > 0 )
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, 
+                transform.localScale.y, transform.localScale.z);
+            //Debug.Log("Tecla A pressionada");
+        }
+        //verifica se a tecla d foi pressionada e o valor x da escala esta negativo
+        if (Input.GetKeyDown(KeyCode.D) && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1,
+                transform.localScale.y, transform.localScale.z);
+            //Debug.Log("Tecla D pressionada");
+        }
+
+
+
     }
 }
